6d1564cd900805126a3ceeaffe5719ffd6774acc
Nizami <nizamializade0@gmail.com>
2017-10-19 16:50:12 +0400

    Add files via upload

--- Trinityy/js/js.js
+++ Trinityy/js/js.js
@@ -1,35 +1,4 @@
 
-// 	function ff(){
-// 	var e = document.getElementById("demo");
-// 	if (e.style.visibility =='visible') {
-// 		e.style.visibility='hidden'
-// 	}
-// 	else{
-// 			e.style.display="none";
-// 	}
-// }
-   $(document).ready(function(){
-        $(".done").click(function(){
-            $(".z").hidden();
-        });
-    });
-
-
-// var acc = document.getElementsByClassName("z");
-// var q = document.getElementsByClassName("testa");
-
-// for ( var i = 1; i < acc.length; i++) {
-//     acc[i].onclick = function(){
-     
-//         // var testa = this.nextElementSibling;
-//         // console.log(testa)
-//         if ( q[i].style.display == "none" || q[i].style.display == "") {
-//            	q[i].style.display = "block";
-//         } else {
-//             q[i].style.display = "none";
-//         }
-//     }
-
 
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
@@ -39,7 +8,9 @@ $(".owl-carousel").owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    dots:true,
     autoplay:true,
+    refresh:true,
 
 
     responsive:{
@@ -47,7 +18,7 @@ $(".owl-carousel").owlCarousel({
             items:1
         },
         600:{
-            items:8
+            items:2
         },
         1000:{
             items:1
